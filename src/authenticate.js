1dc4a6cda40017fdc02489e009fbf39ee022e615
LockBlock-dev <68129141+LockBlock-dev@users.noreply.github.com>
2023-02-01 21:17:03 +0000

    Update authenticate.js

--- src/authenticate.js
+++ src/authenticate.js
@@ -10,7 +10,6 @@ const request = (path, data) => {
       "User-Agent": "UnityPlayer/2020.1.15f1 (UnityWebRequest/1.0, libcurl/7.52.0-DEV)",                  
       "Content-Type": "application/json",
       "Installation": "6081b48e3c12496873c2f003",
-      "Accept-Encoding": "UTF8",
       "X-Unity-Version": "2020.1.15f1"
     }
   }
